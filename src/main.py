1448384f39b5a8460dcf296a2e693a4a07bcac74
Juan Sebastián Osorno Bolívar <juan.osorno@eia.edu.co>
2024-12-04 11:28:04 +0100

    Created a setup file to install the project as a package so that it can recognize the modules. Also I added the project root directory to sys.path

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
+import sys
+import os
+
+# Append project root directory to sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from utils.config import Config
 from utils.data_handler import Datahandler
 
 def main():
+    print("Starting")
     config_path = "config.json"
     config = Config.load_config(config_path)
     config_data = config["data"]
@@ -9,5 +16,6 @@ def main():
     earthquake_file = config_data["earthquake_file"]
     data = Datahandler.load_earthquake_data(earthquake_path, earthquake_file)
     print(data)
+    print("Finishing")
 if __name__ == "main":
     main()
--- tests/test_data_handler.py
+++ tests/test_data_handler.py
@@ -1,8 +1,8 @@
 import pytest
-from utils.data_handler import Datahandler.load_earthquake_data
+from utils.data_handler import Datahandler
 
 def test_load_earthquake_data():
     # Test loading valid data
-    data = load_earthquake_data("test_data/test_data_handler.csv")
+    data = Datahandler.load_earthquake_data("test_data/test_data_handler.csv")
     assert not data.empty
     assert {"X", "Y", "Depth", "Magnitude"}.issubset(data.columns)
