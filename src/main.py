697dfb2934855b618802e7f08a1be14805b6dc2b
Juan Sebastián Osorno Bolívar <juan.osorno@eia.edu.co>
2024-12-06 18:47:45 +0100

    Plotting all profiles in a stacked figure

--- src/main.py
+++ src/main.py
@@ -1,9 +1,4 @@
-import sys
-import os
-
-# Append project root directory to sys.path
-#sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
+import matplotlib.pyplot as plt
 from utils.config import Config
 from utils.data_handler import Datahandler
 from plotter import VerticalSection
@@ -11,27 +6,31 @@ from plotter import VerticalSection
 def main():
     config_path = "config.json"
     config = Config.load_config(config_path)
+    #Data
     config_data = config["data"]
     earthquake_path = config_data["earthquakes_path"]
     earthquake_file = config_data["earthquakes_file"]
+    #Point profiles
     config_point_profiles = config["point_profiles"]
-    profile_start = config_point_profiles["profile_start"]
-    profile_end = config_point_profiles["profile_end"]
-    profile_width = config_point_profiles["profile_width"]
-    profile_depth = config_point_profiles["profile_depth"]
-    datahandler = Datahandler(earthquake_path, earthquake_file, profile_start, profile_end)
-    data = datahandler.load_earthquake_data()
-    print(data)
-    print("All data.")
-    _, filtered_data = datahandler.filter_earthquakes(profile_width, profile_depth)
-    print(filtered_data)
-    print("Filtered data.")
-
     num_profiles = len(config_point_profiles["profile_start"])
-
-    projected_data = datahandler.project_onto_profile(profile_width, profile_depth)
-    plotter = VerticalSection()
-    plotter.draw_cross_section(projected_data, profile_depth, "Cross section BB'")
+    #Initialize figure
+    fig, axes = plt.subplots(nrows = num_profiles, ncols = 1, figsize = (15, 9*num_profiles))
+    for i in range(num_profiles):
+        profile_name = config_point_profiles["profile_name"][i]
+        profile_start = config_point_profiles["profile_start"][i]
+        profile_end = config_point_profiles["profile_end"][i]
+        profile_width = config_point_profiles["profile_width"][i]
+        profile_depth = config_point_profiles["profile_depth"][i]
+        #Datahandler for this profile
+        datahandler = Datahandler(earthquake_path, earthquake_file, profile_start, profile_end) #Instance of datahandler for this profile
+        projected_data = datahandler.project_onto_profile(profile_width, profile_depth)
+        #Plot on specific subplot
+        ax = axes[i] if num_profiles > 1 else axes #Handle single-profile case
+        plotter = VerticalSection()
+        plotter.draw_cross_section(ax, projected_data, profile_depth, profile_name)
+    plt.tight_layout()
+    plt.show()
+    
 
 if __name__ == "__main__":
     main()
